d097f80df969f6adbd3cdfc000c0b8189b1ee678
Mateus Paz <54851249+mateusflorez@users.noreply.github.com>
2023-01-16 20:20:36 -0300

    register page visual update

--- web/src/pages/Register.tsx
+++ web/src/pages/Register.tsx
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom'
+import Logo from '../assets/Logo.png'
 
 function Register() {
     function handleSubmit(e: any) {
@@ -10,18 +11,18 @@ function Register() {
     }
 
     return (
-        <div>
-            <form onSubmit={(e) => handleSubmit(e)}>
-                <div className='brand'>
-                    <img src="" />
-                    <h1 className='text-red-500'>NRChat</h1>
+        <div className='h-[100vh] w-[100vw] flex flex-col justify-center gap-4 items-center bg-[#131324]'>
+            <form onSubmit={(e) => handleSubmit(e)} className='flex flex-col gap-8 bg-[#00000076] rounded-[2rem] py-12 px-20'>
+                <div className='flex items-center gap-4 justify-center'>
+                    <img src={Logo} className='h-20' />
+                    <h1 className='text-white uppercase font-bold text-4xl'>NRChat</h1>
                 </div>
-                <input type="text" placeholder='Username' name='username' onChange={e => handleChange(e)} />
-                <input type="email" placeholder='Email' name='email' onChange={e => handleChange(e)} />
-                <input type="password" placeholder='Password' name='password' onChange={e => handleChange(e)} />
-                <input type="password" placeholder='ConfirmPassword' name='confirmPassword' onChange={e => handleChange(e)} />
-                <button type='submit'>Create user</button>
-                <span>Already have an account? <Link to="/login">Login</Link></span>
+                <input type="text" placeholder='Username' name='username' onChange={e => handleChange(e)} className='bg-transparent p-4 border-[0.1rem] border-[#4e0eff] rounded-[0.4rem] text-white w-full text-base focus:border-[0.1rem] focus:border-[#997af0] focus:outline-none' />
+                <input type="email" placeholder='Email' name='email' onChange={e => handleChange(e)} className='bg-transparent p-4 border-[0.1rem] border-[#4e0eff] rounded-[0.4rem] text-white w-full text-base focus:border-[0.1rem] focus:border-[#997af0] focus:outline-none' />
+                <input type="password" placeholder='Password' name='password' onChange={e => handleChange(e)} className='bg-transparent p-4 border-[0.1rem] border-[#4e0eff] rounded-[0.4rem] text-white w-full text-base focus:border-[0.1rem] focus:border-[#997af0] focus:outline-none' />
+                <input type="password" placeholder='Confirm your password' name='confirmPassword' onChange={e => handleChange(e)} className='bg-transparent p-4 border-[0.1rem] border-[#4e0eff] rounded-[0.4rem] text-white w-full text-base focus:border-[0.1rem] focus:border-[#997af0] focus:outline-none' />
+                <button type='submit' className='bg-[#997af0] text-white py-4 px-8 border-none font-bold cursor-pointer rounded-[0.4rem] text-base uppercase transition hover:bg-[#4e0eff]'>Create user</button>
+                <span className='text-white'>Already have an account? <Link to="/login" className='text-[#4e0eff] no-underline font-bold '>Login</Link></span>
             </form>
         </div>
     )
