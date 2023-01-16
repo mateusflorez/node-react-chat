58830ce6018a42c69e4501db3379128aa1a1947c
Mateus Paz <54851249+mateusflorez@users.noreply.github.com>
2023-01-16 19:50:01 -0300

    css files working

--- web/src/pages/Register.tsx
+++ web/src/pages/Register.tsx
@@ -1,9 +1,32 @@
-import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Register() {
+    function handleSubmit(e: any) {
+        e.preventDefault()
+        alert("form")
+    }
+    function handleChange(e: any) {
+
+    }
+
     return (
-        <div>Register</div>
+        <div>
+            <form onSubmit={(e) => handleSubmit(e)}>
+                <div className='brand'>
+                    <img src="" />
+                    <h1 className='text-red-500'>NRChat</h1>
+                </div>
+                <input type="text" placeholder='Username' name='username' onChange={e => handleChange(e)} />
+                <input type="email" placeholder='Email' name='email' onChange={e => handleChange(e)} />
+                <input type="password" placeholder='Password' name='password' onChange={e => handleChange(e)} />
+                <input type="password" placeholder='ConfirmPassword' name='confirmPassword' onChange={e => handleChange(e)} />
+                <button type='submit'>Create user</button>
+                <span>Already have an account? <Link to="/login">Login</Link></span>
+            </form>
+        </div>
     )
 }
+
+
 
 export default Register
