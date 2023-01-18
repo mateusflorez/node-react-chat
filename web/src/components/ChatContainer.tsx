285a2b84d439ed43726d60acbb4a40707a8d1d3f
Mateus Paz <54851249+mateusflorez@users.noreply.github.com>
2023-01-18 10:18:52 -0300

    chat container header

---
+++ web/src/components/ChatContainer.tsx
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react"
+
+
+export default function ChatContainer({ currentChat }: { currentChat: any }) {
+  const [currentChatname, setCurrentChatname] = useState([undefined])
+  const [currentChatImage, setCurrentChatImage] = useState([undefined])
+
+  useEffect(() => {
+    if (currentChat) {
+      setCurrentChatImage(currentChat.avatarImage)
+      setCurrentChatname(currentChat.username)
+    }
+  }, [currentChat])
+
+  return (
+    <div className="pt-4">
+      <div className="flex justify-between items-center px-8">
+        <div className="flex items-center gap-4">
+          <div>
+            <img className='h-12' src={`data:image/svg+xml;base64,${currentChatImage}`} />
+          </div>
+          <div>
+            <h2 className="text-white font-bold text-base">{currentChatname}</h2>
+          </div>
+        </div>
+      </div>
+      <div></div>
+      <div></div>
+    </div>
+  )
+}
--- web/src/pages/Chat.tsx
+++ web/src/pages/Chat.tsx
@@ -2,8 +2,9 @@ import { useState, useEffect } from "react"
 import axios from "axios"
 import { useNavigate } from "react-router-dom"
 import { allUsersRoute } from "../utils/APIRoutes"
-import Contacts from "../components/contacts"
+import Contacts from "../components/Contacts"
 import Welcome from "../components/Welcome"
+import ChatContainer from "../components/ChatContainer"
 
 
 function Chat() {
@@ -47,7 +48,13 @@ function Chat() {
         <div className="h-[100vh] w-[100vw] flex flex-col justify-center gap-4 items-center bg-night">
             <div className="h-[85vh] w-[85vw] bg-ghost grid grid-cols-[50%_50%] sm:grid-cols-[35%_65%] rounded-[0.4rem] overflow-hidden">
                 <Contacts contacts={contacts} currentUser={currentUser} changeChat={handleChatChange} />
-                <Welcome currentUser={currentUser} />
+                {
+                    currentChat === undefined ? (
+                        <Welcome currentUser={currentUser} />
+                    ) : (
+                        <ChatContainer currentChat={currentChat} />
+                    )
+                }
             </div>
         </div>
     )
